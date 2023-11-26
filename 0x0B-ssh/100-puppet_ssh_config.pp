6090079e86dc08da0927b5d2b594fd8ff24a67ac
Atala2000 <atalakidi@gmail.com>
2023-11-27 00:47:24 +0300

    putty

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 # puppet script
 file_line { 'Turn off passwd auth':
-  ensure => 'present,
-  path => '/etc/ssh/ssh_config',
-  line => '    PasswordAuthentication no'
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
 }
 
 file_line { 'Declare identity file':
-  esnure => 'present',
-  path => '/etc/ssh/ssh_config',
-  line => '    identityFile ~/.ssh/school',
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
 }
