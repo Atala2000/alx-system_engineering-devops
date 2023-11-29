0ca22cc08dd5ebbd1a7a4e577365be630a98e072
Atala2000 <atalakidi@gmail.com>
2023-11-29 21:07:34 +0300

    tasks

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+# install server
+
+package { 'nginx':
+  ensure => installed,
+}
+
+# Ensure Nginx service is running and enabled
+service { 'nginx':
+  ensure => running,
+  enable => true,
+}
+
+# Configure Nginx site
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => "
+    server {
+      listen 80 default_server;
+      listen [::]:80 default_server;
+
+      root /var/www/html;
+      index index.html index.htm index.nginx-debian.html;
+
+      server_name _;
+
+      location / {
+        return 200 'Hello World!';
+      }
+
+      location /redirect_me {
+        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+      }
+    }
+  ",
+  notify  => Service['nginx'],
+}
+
+# Create the Hello World page
+file { '/var/www/html/index.nginx-debian.html':
+  ensure  => file,
+  content => 'Hello World!',
+}
