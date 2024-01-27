dda25d9d7e580dba7e12a7fcc0ece3ee41b1153d
Atala2000 <atalakidi@gmail.com>
2024-01-28 01:21:45 +0300

    0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,13 @@
-# Increases the amount of traffic 
+#changing the ULIMIT
 
-# Increase the ULIMIT of the default file
-exec { 'fix--for-nginx':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/'
-} ->
+exec { 'changing ULIMIT':
+command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
 
-# Restart Nginx
-exec { 'nginx-restart':
-  command => 'nginx restart',
-  path    => '/etc/init.d/'
+path    => '/usr/local/bin/:/bin/',
+}
+
+exec { 'restarting server':
+command => '/etc/init.d/nginx restart',
+
+path    => '/etc/init.d/',
 }
